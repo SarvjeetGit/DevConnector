ce8e702e10918fc5d3c8520506ade13185627248
SarvjeetGit <sarvjeetrocks@gmail.com>
2021-03-12 11:47:11 +0530

    Developing FrontEnd

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth';
@@ -42,6 +43,11 @@ const App = () => {
                                 exact
                                 path='/dashboard'
                                 component={Dashboard}
+                            />
+                            <PrivateRoute
+                                exact
+                                path='/create-profile'
+                                component={CreateProfile}
                             />
                         </Switch>
                     </section>

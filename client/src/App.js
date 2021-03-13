59f700fbf9fc470812fc2aa59890883247817127
SarvjeetGit <sarvjeetrocks@gmail.com>
2021-03-13 13:30:39 +0530

    add education done

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth';
@@ -60,6 +61,11 @@ const App = () => {
                                 exact
                                 path='/add-experience'
                                 component={AddExperience}
+                            />
+                            <PrivateRoute
+                                exact
+                                path='/add-education'
+                                component={AddEducation}
                             />
                         </Switch>
                     </section>

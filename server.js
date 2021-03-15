0e2fc143ee8c010d57e97db7bc43a099a635d577
SarvjeetGit <sarvjeetrocks@gmail.com>
2021-03-16 03:03:46 +0530

    prepared for deploy

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 //connect database
 connectDB();
-
-app.get('/', (req, res) => {
-    res.send('API running');
-});
-
 //Initialise middleware
 app.use(express.json({ extended: false }));
 
@@ -16,6 +12,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//Serve Static Assets
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
